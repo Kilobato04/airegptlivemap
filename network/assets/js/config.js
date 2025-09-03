1e2c173a837092bd273b6c5a3e2cce3d9629dc30
Kilobato04 <134810631+Kilobato04@users.noreply.github.com>
2025-09-03 15:59:29 -0600

    Update config.js

--- network/assets/js/config.js
+++ network/assets/js/config.js
@@ -11,25 +11,30 @@ window.APP_SETTINGS = {
     refreshInterval: 3000,
     maxRetries: 3,
     activeStations: [
-        'MicroSensor-01',
-        'MicroSensor-02', 
-        'MicroSensor-03',
+        // 'MicroSensor-01',    // EN REPARACIÓN
+        // 'MicroSensor-02',    // EN REPARACIÓN 
+        // 'MicroSensor-03',    // EN REPARACIÓN
         'UNAM',
-        'CENTRUS 8',
-        'CENTRUS 7',
+        // 'CENTRUS 8',         // EN REPARACIÓN
+        // 'CENTRUS 7',         // EN REPARACIÓN
         'Del Valle',
         'CENTRUS 5', 
         'Huerto IBERO',
         'INIAT',
-        'IBERO A',
-        'CENTRUS 1',
-        'CENTRUS 2',
+        // 'IBERO A',           // EN REPARACIÓN
+        // 'CENTRUS 1',         // EN REPARACIÓN
+        // 'CENTRUS 2',         // EN REPARACIÓN
         'La Diana',
-        'CENTRUS 4',
+        // 'CENTRUS 4',         // EN REPARACIÓN
         'Hipódromo',
         'UAM',
         'INSYC',
-        'UNAM-II-SMAA'
+        // 'UNAM-II-SMAA',      // EN REPARACIÓN
+        'Anahuac Cancun',       // NUEVO
+        'ITD',                  // NUEVO
+        'Tepeji',               // NUEVO
+        'AlisBio',              // NUEVO
+        'AlisBio2'              // NUEVO - ACTIVO
     ],
     whatsappNumber: '525519566483',
     brandUrl: 'http://www.smability.io'
@@ -54,49 +59,59 @@ window.API_CONFIG = {
     baseUrl: 'http://smability.sidtecmx.com/SmabilityAPI/BioBox',
     dataUrl: 'http://smability.sidtecmx.com/SmabilityAPI/GetData',
     tokens: {
-        'MicroSensor-01': 'b9f56c2a86c59c27e23b93126c508abe',
-        'MicroSensor-02': '941952e3aaee033b7d4a126ef7194a45',
-        'MicroSensor-03': 'e25b9d53259785daf60e520834d3f39f',
+        // 'MicroSensor-01': 'b9f56c2a86c59c27e23b93126c508abe',    // EN REPARACIÓN
+        // 'MicroSensor-02': '941952e3aaee033b7d4a126ef7194a45',    // EN REPARACIÓN
+        // 'MicroSensor-03': 'e25b9d53259785daf60e520834d3f39f',    // EN REPARACIÓN
         'UNAM': '349b1230277f1c67577e4f5bee6ba486',
-        'CENTRUS 8': 'a2a9a62b622014c64426c6484334e577',
-        'CENTRUS 7': '510a41e183a90482275b1bc8a79dfc14',
+        // 'CENTRUS 8': 'a2a9a62b622014c64426c6484334e577',         // EN REPARACIÓN
+        // 'CENTRUS 7': '510a41e183a90482275b1bc8a79dfc14',         // EN REPARACIÓN
         'Del Valle': '1ae5a53b7b315c48206ffa95fdef32d8',
         'CENTRUS 5': 'c7dd6160528b3335d4f366e28a04ac80',
         'Huerto IBERO': 'b8c1bac206b358bde62cb25c374339c3',
         'INIAT': '211b4d2dd56a0ba2eb269f2eeb0945ea',
-        'IBERO A': '1ce05f7c4d1e7a2f048bf5256285a276',
-        'CENTRUS 1': '60374b0d5f818b9f7703cc1a27aeab48',
-        'CENTRUS 2': '993a85c50d07d8a3c367f32d1a3d12af',
+        // 'IBERO A': '1ce05f7c4d1e7a2f048bf5256285a276',           // EN REPARACIÓN
+        // 'CENTRUS 1': '60374b0d5f818b9f7703cc1a27aeab48',         // EN REPARACIÓN
+        // 'CENTRUS 2': '993a85c50d07d8a3c367f32d1a3d12af',         // EN REPARACIÓN
         'La Diana': '3843693a0dc1e3aca614cf3e71976527',
-        'CENTRUS 4': 'c0ac14b4df16d24f3e403928412fdc31',
+        // 'CENTRUS 4': 'c0ac14b4df16d24f3e403928412fdc31',         // EN REPARACIÓN
         'Hipódromo': '1c5e12e8f00c9f2cbb4c7c8f07c9d398',
         'UAM': 'b031aca25a4746368da8e718631e06e7',
         'INSYC': '3d820ec8b5149f835f6c5cc338d247e2',
-        'UNAM-II-SMAA': '03e85a1e71f0b9ffaf041862303b250b'
+        // 'UNAM-II-SMAA': '03e85a1e71f0b9ffaf041862303b250b',       // EN REPARACIÓN
+        'Anahuac Cancun': 'b1a3d78281c7e0cd6cd56dad7d83353c',       // NUEVO
+        'ITD': '005cdaeb7b1392ad59a4335f4a832043',                   // NUEVO
+        'Tepeji': 'b44eebc1879479e4534abddccef2d0fc',               // NUEVO
+        'AlisBio': '2a53e9174dcf7ee42b9c8bbda7990571',              // NUEVO
+        'AlisBio2': '3f3b878fdb02f61d5ed43168c075cec8'              // NUEVO - ACTIVO
     }
 };
 
 // ADDED: Device coordinates for programmatic marker creation
 window.DEVICE_COORDINATES = {
-    'MicroSensor-01': [-99.262583, 19.370218],
-    'MicroSensor-02': [-99.262952, 19.370842],
-    'MicroSensor-03': [-99.263291, 19.371326],
+    // 'MicroSensor-01': [-99.262583, 19.370218],    // EN REPARACIÓN
+    // 'MicroSensor-02': [-99.262952, 19.370842],    // EN REPARACIÓN
+    // 'MicroSensor-03': [-99.263291, 19.371326],    // EN REPARACIÓN
     'UNAM': [-99.176186, 19.326172],
-    'CENTRUS 8': [-99.173172, 19.392838],
-    'CENTRUS 7': [-99.172752, 19.392725],
+    // 'CENTRUS 8': [-99.173172, 19.392838],         // EN REPARACIÓN
+    // 'CENTRUS 7': [-99.172752, 19.392725],         // EN REPARACIÓN
     'Del Valle': [-99.167000, 19.388391],
     'CENTRUS 5': [-99.200109, 19.365869],
     'Huerto IBERO': [-99.263649, 19.372347],
     'INIAT': [-99.263580, 19.368851],
-    'IBERO A': [-99.263649, 19.372355],
-    'CENTRUS 1': [-99.263489, 19.368654],
-    'CENTRUS 2': [-99.263626, 19.368771],
+    // 'IBERO A': [-99.263649, 19.372355],           // EN REPARACIÓN
+    // 'CENTRUS 1': [-99.263489, 19.368654],         // EN REPARACIÓN
+    // 'CENTRUS 2': [-99.263626, 19.368771],         // EN REPARACIÓN
     'La Diana': [-99.171021, 19.425217],
-    'CENTRUS 4': [-99.262962, 19.371620],
+    // 'CENTRUS 4': [-99.262962, 19.371620],         // EN REPARACIÓN
     'Hipódromo': [-99.167183, 19.414782],
     'UAM': [-99.209564, 19.388735],
     'INSYC': [-99.169975, 19.406158],
-    'UNAM-II-SMAA': [-99.176163, 19.326214]
+    // 'UNAM-II-SMAA': [-99.176163, 19.326214],      // EN REPARACIÓN
+    'Anahuac Cancun': [-99.168274, 19.417080],       // NUEVO
+    'ITD': [-104.645030, 24.034481],                  // NUEVO
+    'Tepeji': [-99.341911, 19.905193],               // NUEVO
+    'AlisBio': [-100.290960, 25.675648],             // NUEVO
+    'AlisBio2': [-100.309264, 25.667362]             // NUEVO - ACTIVO (coordenadas del JSON)
 };
 
 // AGREGADO: Configuración de sensores para charts
