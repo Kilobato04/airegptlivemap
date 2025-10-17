4cd165e09c264f111316b7140dc5cc5c68855c83
Kilobato04 <134810631+Kilobato04@users.noreply.github.com>
2025-10-17 10:22:08 -0600

    Update config.js

--- network/assets/js/config.js
+++ network/assets/js/config.js
@@ -97,7 +97,7 @@ window.MASTER_API_CONFIG = {
  */
 window.REFERENCE_STATION_MAPPING = {
     // SOLO UNA ESTACIÓN PARA TESTING
-    'TLI': 'Tultitlán'
+    'TLI': 'Tultitlán',
     
     // COMENTADAS para testing - descomentar después:
     // 'CUA': 'Cuajimalpa', 
