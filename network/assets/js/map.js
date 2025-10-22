511b5e73493fd1a2baecf38f28a44b7ba140e7f5
Kilobato04 <134810631+Kilobato04@users.noreply.github.com>
2025-10-22 14:22:37 -0600

    Update map.js

--- network/assets/js/map.js
+++ network/assets/js/map.js
@@ -273,7 +273,7 @@ setTimeout(() => {
             'layout': {
                 'text-field': '■',
                 'text-font': ['Open Sans Bold', 'Arial Unicode MS Bold'],
-                'text-size': 40, // 4px más grande que el principal (29 + 4)
+                'text-size': 48, // 4px más grande que el principal (29 + 4)
                 'text-allow-overlap': true,
                 'text-ignore-placement': true
             },
