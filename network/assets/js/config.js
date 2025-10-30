1ef0aaf5a8a9e37ddc641fdb68c1227601fb456c
Kilobato04 <134810631+Kilobato04@users.noreply.github.com>
2025-10-30 16:48:59 -0600

    Update config.js

--- network/assets/js/config.js
+++ network/assets/js/config.js
@@ -19,22 +19,22 @@ window.APP_SETTINGS = {
         // 'CENTRUS 7',         // EN REPARACIÓN
         'D.Valle',
         'CEN 5', 
-        'Huerto INIAT',
-        'INIAT IBERO',
+        // 'Huerto INIAT',
+        // 'INIAT IBERO',
         // 'IBERO A',           // EN REPARACIÓN
-        'Zacatlan 1',         // MINA
-        'Zacatlan 2',         // MINA
+        // 'Zacatlan 1',         // MINA
+        // 'Zacatlan 2',         // MINA
         // 'La Diana',        // EN REPARACIÓN
         // 'CENTRUS 4',         // EN REPARACIÓN
         // 'Popocatépetl',        // EN REPARACIÓN
         //  'UAM',                // EN REPARACIÓN
         //  'INSYC',            // EN REPARACIÓN
         // 'UNAM-II-SMAA',      // EN REPARACIÓN
-        'Anahuac-Cancun',       // NUEVO
-        'ITD-Durango',                  // NUEVO
+        // 'Anahuac-Cancun',       // NUEVO
+        // 'ITD-Durango',                  // NUEVO
         // 'Tepeji',            // EN REPARACIÓN
-        'Alis-Bio',              // NUEVO
-        'Alis-Bio2'              // NUEVO - ACTIVO
+        // 'Alis-Bio',              // NUEVO
+        // 'Alis-Bio2'              // NUEVO - ACTIVO
     ],
     whatsappNumber: '525519566483',
     brandUrl: 'http://www.smability.io'
@@ -67,22 +67,22 @@ window.API_CONFIG = {
         // 'CENTRUS 7': '510a41e183a90482275b1bc8a79dfc14',         // EN REPARACIÓN
         'D.Valle': '1ae5a53b7b315c48206ffa95fdef32d8',
         'CEN 5': 'c7dd6160528b3335d4f366e28a04ac80',
-        'Huerto INIAT': 'b8c1bac206b358bde62cb25c374339c3',
-        'INIAT IBERO': '211b4d2dd56a0ba2eb269f2eeb0945ea',
+        // 'Huerto INIAT': 'b8c1bac206b358bde62cb25c374339c3',
+        // 'INIAT IBERO': '211b4d2dd56a0ba2eb269f2eeb0945ea',
         // 'IBERO A': '1ce05f7c4d1e7a2f048bf5256285a276',           // EN REPARACIÓN
         // 'CENTRUS 1': '60374b0d5f818b9f7703cc1a27aeab48',         // EN REPARACIÓN
-        'Zacatlan 1': '993a85c50d07d8a3c367f32d1a3d12af',         // MINA
+        // 'Zacatlan 1': '993a85c50d07d8a3c367f32d1a3d12af',         // MINA
         // 'La Diana': '3843693a0dc1e3aca614cf3e71976527',
-        'Zacatlan 2': 'c0ac14b4df16d24f3e403928412fdc31',         // MINA
+        // 'Zacatlan 2': 'c0ac14b4df16d24f3e403928412fdc31',         // MINA
         //'Popocatépetl': '1c5e12e8f00c9f2cbb4c7c8f07c9d398',
         // 'UAM': 'b031aca25a4746368da8e718631e06e7',
         // 'INSYC': '3d820ec8b5149f835f6c5cc338d247e2',
         // 'UNAM-II-SMAA': '03e85a1e71f0b9ffaf041862303b250b',       // EN REPARACIÓN
-        'Anahuac-Cancun': 'b1a3d78281c7e0cd6cd56dad7d83353c',       // NUEVO
-        'ITD-Durango': '005cdaeb7b1392ad59a4335f4a832043',                   // NUEVO
+        // 'Anahuac-Cancun': 'b1a3d78281c7e0cd6cd56dad7d83353c',       // NUEVO
+        // 'ITD-Durango': '005cdaeb7b1392ad59a4335f4a832043',                   // NUEVO
         // 'Tepeji': 'b44eebc1879479e4534abddccef2d0fc',               // NUEVO
-        'Alis-Bio': '2a53e9174dcf7ee42b9c8bbda7990571',              // NUEVO
-        'Alis-Bio2': '3f3b878fdb02f61d5ed43168c075cec8'              // NUEVO - ACTIVO
+        // 'Alis-Bio': '2a53e9174dcf7ee42b9c8bbda7990571',              // NUEVO
+        // 'Alis-Bio2': '3f3b878fdb02f61d5ed43168c075cec8'              // NUEVO - ACTIVO
     }
 };
 
@@ -96,22 +96,22 @@ window.DEVICE_COORDINATES = {
     // 'CENTRUS 7': [-99.172752, 19.392725],         // EN REPARACIÓN
     'D.Valle': [-99.167000, 19.388391],
     'CEN 5': [-99.200109, 19.365869],
-    'Huerto INIAT': [-99.263649, 19.372347],
-    'INIAT IBERO': [-99.263580, 19.368851],
+    // 'Huerto INIAT': [-99.263649, 19.372347],
+    // 'INIAT IBERO': [-99.263580, 19.368851],
     // 'IBERO A': [-99.263649, 19.372355],           // EN REPARACIÓN
     // 'CENTRUS 1': [-99.263489, 19.368654],         // EN REPARACIÓN
-    'Zacatlan 1': [-98.058182, 19.984287],         // MINA
+    // 'Zacatlan 1': [-98.058182, 19.984287],         // MINA
     // 'La Diana': [-99.171021, 19.425217],
-    'Zacatlan 2': [-98.058884, 19.963730],         // MINA 
+    // 'Zacatlan 2': [-98.058884, 19.963730],         // MINA 
     // 'Popocatépetl': [-99.167183, 19.414782],
     // 'UAM': [-99.209564, 19.388735],
     // 'INSYC': [-99.169975, 19.406158],
     // 'UNAM-II-SMAA': [-99.176163, 19.326214],      // EN REPARACIÓN
-    'Anahuac-Cancun': [-86.84543905576808, 21.067920239176036],
-    'ITD-Durango': [-104.645030, 24.034481],                  // NUEVO
+    // 'Anahuac-Cancun': [-86.84543905576808, 21.067920239176036],
+    // 'ITD-Durango': [-104.645030, 24.034481],                  // NUEVO
     // 'Tepeji': [-99.341911, 19.905193],               // NUEVO
-    'Alis-Bio': [-100.290960, 25.675648],             // NUEVO
-    'Alis-Bio2': [-99.995773, 25.382708]             // NUEVO - ACTIVO (coordenadas del JSON) 
+    // 'Alis-Bio': [-100.290960, 25.675648],             // NUEVO
+    // 'Alis-Bio2': [-99.995773, 25.382708]             // NUEVO - ACTIVO (coordenadas del JSON) 
 };
 
 // AGREGADO: Mapeo de modelos de dispositivos
