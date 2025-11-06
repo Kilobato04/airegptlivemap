524e60a740a084b89681d4dfceeed3cb405b0085
Kilobato04 <134810631+Kilobato04@users.noreply.github.com>
2025-11-06 09:21:24 -0600

    Update station names in map.js

--- network/assets/js/map.js
+++ network/assets/js/map.js
@@ -549,7 +549,7 @@ function addMapLayers() {
                     'Del Valle', 'Huerto IBERO', 'CENTRUS 2', 'CENTRUS 4', 
                     'INIAT', 'CENTRUS 5', 'ITD', 'ALISBio-02', 'ALISBio', 
                     'MicroSensor-03', 'Anahuac Cancun', 'MicroSensor-02',
-                    'INSYC-Smability', 'Hipódromo', 'La Diana', 'UNAM - ICAYCC', 'UAM', 'Tepeji', // ← NUEVAS
+                    'INSYC-Smability', 'Hipódromo', 'La Diana', 'UNAM - ICAYCC', 'UAM SF IMSS', 'Tepeji HGO', // ← NUEVAS
                     
                     // Estaciones Reference activas (22 estaciones):
                     'Cuautitlan', 'Merced', 'UAM Xochimilco', 'Atizapan', 'Tlalnepantla',
@@ -610,7 +610,7 @@ function addMapLayers() {
                 'Del Valle', 'Huerto IBERO', 'CENTRUS 2', 'CENTRUS 4', 
                 'INIAT', 'CENTRUS 5', 'ITD', 'ALISBio-02', 'ALISBio', 
                 'MicroSensor-03', 'Anahuac Cancun', 'MicroSensor-02',
-                'INSYC-Smability', 'Hipódromo', 'La Diana', 'UNAM - ICAYCC', 'UAM', 'Tepeji', // ← NUEVAS
+                'INSYC-Smability', 'Hipódromo', 'La Diana', 'UNAM - ICAYCC', 'UAM SF IMSS', 'Tepeji HGO', // ← NUEVAS
                 'Cuautitlan', 'Merced', 'UAM Xochimilco', 'Atizapan', 'Tlalnepantla',
                 'Santiago Acahualtepec', 'Hospital General de Mexico', 'Ajusco Medio',
                 'Centro de Ciencias de la Atmosfera', 'FES Acatlan', 'Camarones',
